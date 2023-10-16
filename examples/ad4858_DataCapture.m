366160e02bb1a9c72f22367e1973f80c2a4969a5
RibhuDP <ribhu.daspurkayastha@analog.com>
2023-10-16 12:19:07 +0530

    examples:ad4858: Remove comments and property
    1. Remove comments not applicable to AD4858
    2. Remove reference to unused FramesCount property
    
    Signed-off-by: RibhuDP <ribhu.daspurkayastha@analog.com>

--- examples/ad4858_DataCapture.m
+++ examples/ad4858_DataCapture.m
@@ -1,9 +1,6 @@
 %% Script for capturing and displaying a continuous set of samples from a 
 %% connected AD4858 board
 
-%% Trigger setup needs to be performed before this example script can be run
-%% Refer to the Limitations section in the documentation for details
-
 % Instantiate the system object
 rx = adi.AD4858.Rx;
 % Specify uri
@@ -11,11 +8,6 @@ rx.uri = 'ip:analog.local';
 rx.SamplesPerFrame = 4096;  % Using values less than 3660 can yield poor 
 % performance, generally
 rx.EnabledChannels = [1 2 3 4 5 6 7 8];
-
-% The parameter below specifies the number of frames or buffers to capture.
-% Refer to the Streaming section in the documentation if discontinuities
-% are observed in the acquired data.
-rx.FrameCount = 1;
 
 % Capture data
 data = rx();
