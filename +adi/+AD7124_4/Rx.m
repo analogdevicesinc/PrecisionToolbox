76240ce259bc1b6ef975a405c0ee9f389294113b
Alexandru Ardelean <aardelean@baylibre.com>
2024-11-01 18:43:07 +0200

    AD7124-{4,8}: adjust channel names for both parts
    In a newer version of the AD7124 driver, the channels were all made
    differential.
    That means that the channel names need to be updated.
    
    This change updates the channel names to the new (differential channel
    names).
    
    Signed-off-by: Alexandru Ardelean <aardelean@baylibre.com>

--- +adi/+AD7124_4/Rx.m
+++ +adi/+AD7124_4/Rx.m
@@ -12,8 +12,8 @@ classdef Rx < adi.common.Rx & adi.common.RxTx ...
     % `AD7124-4 Datasheet <https://www.analog.com/media/en/technical-documentation/data-sheets/ad7124-4.pdf>`_
 
     properties (Nontunable, Hidden)
-        channel_names = {'voltage0','voltage1','voltage2'...
-            'voltage3','voltage4','voltage5','voltage6','voltage7'};
+        channel_names = {'voltage0-voltage1','voltage2-voltage3',...
+            'voltage4-voltage5','voltage6-voltage7'};
     end
 
     methods
