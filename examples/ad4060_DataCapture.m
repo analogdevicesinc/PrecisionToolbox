dce3b98247a030bad9fbb44e45f8a9e357b9c03b
Swaroop PrakashKumar <Swaroop.PrakashKumar@analog.com>
2025-07-23 15:03:38 +0530

    adi:ad4060:Added Matlab Support for AD4060
    Signed-off-by: Swaroop PrakashKumar <Swaroop.PrakashKumar@analog.com>

---
+++ examples/ad4060_DataCapture.m
@@ -0,0 +1,23 @@
+%% Script for capturing and displaying a continuous set of samples from a 
+%% connected AD4060 board
+
+% Instantiate the system object
+rx = adi.AD4060.Rx('uri', 'serial:COM8,230400');
+rx.SamplesPerFrame = 400; 
+rx.EnabledChannels = [1];
+
+rx.SampleRate = 62500; % In sample mode, the sampling rate can be as high as 140KSPS
+
+% Capture data
+data = rx();
+
+enabledChannels = size(data, 2);
+figure(1);
+for i = 1:enabledChannels
+    subplot(enabledChannels, 1, i);
+    plot(data(1:rx.SamplesPerFrame, i));
+    title("Channel " + num2str(rx.EnabledChannels(i)));
+end
+
+% Delete the system object
+release(rx);
